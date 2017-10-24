bfa8db8903fbf7c7091976542f02faadcd45e0ce
jmenaguale <jmena1@u.brockport.edu>
2017-10-24 19:11:17 -0400

    added initial sql code

--- config.php
+++ config.php
@@ -3,11 +3,12 @@
  * Configuration for database connection
  *
  */
-$host       = "localhost"; //???
+ //Not sure if these fields are right
+$host       = "csdb.brockport.edu";
 $username   = "kdarl1";
 $password   = "webdesign";
-$dbname     = "fal17_csc423_kdarl1";
-$dsn        = "mysql:host=$host;dbname=$dbname"; //???
+$dbname     = "test";
+$dsn        = "mysql:host=$host;dbname=$dbname";
 $options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
               );
