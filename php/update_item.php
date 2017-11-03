7d405d7bd906526ad0df859226041f662629fd00
Kyle <kdarling95@yahoo.com>
2017-11-03 19:47:47 -0400

    Finished Edit modal and validation checks
    Currently form does not launch, no clue why.

--- php/update_item.php
+++ php/update_item.php
@@ -1,5 +1,5 @@
 <!-- insert item
-	Script that allows the item to be inserted into the database based on 
+	Script that allows the item to be inserted into the database based on
 	requirements provided
 -->
 
@@ -12,19 +12,20 @@ update_item();
 
 function update_item() {
 
-	// connect to the main database where the items are stored	
+	// connect to the main database where the items are stored
 	connect_to_db(DB_SERVER, DB_UN, DB_PWD, DB_NAME);
-	$item_number = $_POST['itemNumber'];	
+	$item_number = $_POST['itemNumber'];
 	$item_description = $_POST['description'];
 	$item_category = $_POST['category'];
 	$department_name = $_POST['department'];
 	$purchase_cost = $_POST['purchaseCost'];
 	$retail_price = $_POST['retailPrice'];
-	
+
 	// append string
 	$appendString="";
-	
+
 	// setup string
+	if($item_number != '')			$appendString .= "ItemNumber='$item_number', ";
 	if($item_description != '') $appendString .= "ItemDescription='$item_description', ";
 	if($item_category != '') $appendString .= "Category='$item_category', ";
 	if($department_name != '') $appendString .= "DepartmentName='$department_name', ";
@@ -68,8 +69,8 @@ function connect_to_db($server, $username, $pwd, $dbname) {
 	$dbh = mysql_select_db($dbname);
 	if(!$dbh) {
 		echo "Unable to select " .$dbname. ": " . mysql_error();
-		exit; 
+		exit;
 	}
 }
 
-?> 
+?>
