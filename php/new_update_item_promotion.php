2865b8a3c3dafa3590820bb5beb30de1b708b73d
Kyle <kdarling95@yahoo.com>
2017-11-16 17:38:48 -0500

    Item Search Promotion GUI is Complete

---
+++ php/new_update_item_promotion.php
@@ -0,0 +1,74 @@
+<!--
+  Adds the item to the specified promotion
+-->
+<?php
+require('server_connection.inc');
+
+require('display_result.inc');
+
+update_item_promtion();
+
+function update_item_promotion() {
+
+	// connect to the main database where the items are stored
+	connect_to_db(DB_SERVER, DB_UN, DB_PWD, DB_NAME);
+	$item_number = $_POST['itemNumber'];
+	$item_description = $_POST['description'];
+	$item_category = $_POST['category'];
+	$department_name = $_POST['department'];
+	$purchase_cost = $_POST['cost'];
+	$retail_price = $_POST['price'];
+
+	// append string
+	$appendString="";
+
+	// setup string
+	if($item_number != '')			$appendString .= "ItemNumber='$item_number', ";
+	if($item_description != '') $appendString .= "ItemDescription='$item_description', ";
+	if($item_category != '') $appendString .= "Category='$item_category', ";
+	if($department_name != '') $appendString .= "DepartmentName='$department_name', ";
+	if($purchase_cost != '') $appendString .= "PurchaseCost='$purchase_cost', ";
+	if($retail_price != '') $appendString .= "FullRetailPrice='$retail_price', ";
+
+	$appendString = str_lreplace(",","",$appendString);
+	// create the statement
+	$insertStatement = "update Item set $appendString where ItemNumber='$item_number'";
+
+	$result = mysql_query($insertStatement);
+
+	$message = "";
+
+	if(!$result) {
+		$message = "Error in updating Item: $item_number: ". mysql_error();
+	} else {
+		$message = "Item Successfully Updated for ItemNumber: $item_number.";
+	}
+	display_result_item($message);
+}
+
+function str_lreplace($search, $replace, $subject)
+{
+    $pos = strrpos($subject, $search);
+
+    if($pos !== false)
+    {
+        $subject = substr_replace($subject, $replace, $pos, strlen($search));
+    }
+
+    return $subject;
+}
+
+function connect_to_db($server, $username, $pwd, $dbname) {
+	$conn = mysql_connect($server, $username, $pwd);
+	if(!$conn) {
+			display_result_item("Unable to connect to DB: " . mysql_error());
+			exit;
+	}
+	$dbh = mysql_select_db($dbname);
+	if(!$dbh) {
+		display_result_item("Unable to select " .$dbname. ": " . mysql_error());
+		exit;
+	}
+}
+
+?>
